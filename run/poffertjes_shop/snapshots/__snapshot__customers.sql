d4b94fd4853cf480abcf84f45782303b31ae2122
tjwaterman99 <tjwaterman99@users.noreply.github.com>
2022-02-03 17:09:49 +0000

    deploy: a36f575f100c8316c15618b5ee875dfce10aef80

---
+++ run/poffertjes_shop/snapshots/__snapshot__customers.sql
@@ -0,0 +1,14 @@
+
+      update "poffertjes_shop"."poffertjes_snapshots"."customers_snapshot"
+    set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to
+    from "customers_snapshot__dbt_tmp170943180664" as DBT_INTERNAL_SOURCE
+    where DBT_INTERNAL_SOURCE.dbt_scd_id::text = "poffertjes_shop"."poffertjes_snapshots"."customers_snapshot".dbt_scd_id::text
+      and DBT_INTERNAL_SOURCE.dbt_change_type::text in ('update'::text, 'delete'::text)
+      and "poffertjes_shop"."poffertjes_snapshots"."customers_snapshot".dbt_valid_to is null;
+
+    insert into "poffertjes_shop"."poffertjes_snapshots"."customers_snapshot" ("customer_id", "contact", "name", "created_at", "updated_at", "dbt_updated_at", "dbt_valid_from", "dbt_valid_to", "dbt_scd_id")
+    select DBT_INTERNAL_SOURCE."customer_id",DBT_INTERNAL_SOURCE."contact",DBT_INTERNAL_SOURCE."name",DBT_INTERNAL_SOURCE."created_at",DBT_INTERNAL_SOURCE."updated_at",DBT_INTERNAL_SOURCE."dbt_updated_at",DBT_INTERNAL_SOURCE."dbt_valid_from",DBT_INTERNAL_SOURCE."dbt_valid_to",DBT_INTERNAL_SOURCE."dbt_scd_id"
+    from "customers_snapshot__dbt_tmp170943180664" as DBT_INTERNAL_SOURCE
+    where DBT_INTERNAL_SOURCE.dbt_change_type::text = 'insert'::text;
+
+  
