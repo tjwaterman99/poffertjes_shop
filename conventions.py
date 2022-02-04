36749f487e3efb76e46c2ed98948199ca817ffe2
Tom Waterman <tjwaterman99@gmail.com>
2022-02-04 07:58:49 +0000

    More WIP

--- conventions.py
+++ conventions.py
@@ -2,3 +2,7 @@
 
 def test_selectors_have_descriptions(selector):
     assert selector.get('description') is not None
+
+
+def test_macros_have_descriptions(macro):
+    assert macro.description is not None
