857a2647235ebd433b7b0f3163195a61c422f792
tjwaterman99 <tjwaterman99@users.noreply.github.com>
2022-02-06 13:37:29 +0000

    deploy: 2a39803d33e2bc7fa7a27b289f4d97fe230c1c13

--- run/poffertjes_shop/snapshots/__snapshot__order_items.sql
+++ run/poffertjes_shop/snapshots/__snapshot__order_items.sql
@@ -1,14 +1,14 @@
 
       update "poffertjes_shop"."poffertjes_snapshots"."order_items_snapshot"
     set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to
-    from "order_items_snapshot__dbt_tmp131755219684" as DBT_INTERNAL_SOURCE
+    from "order_items_snapshot__dbt_tmp133721025129" as DBT_INTERNAL_SOURCE
     where DBT_INTERNAL_SOURCE.dbt_scd_id::text = "poffertjes_shop"."poffertjes_snapshots"."order_items_snapshot".dbt_scd_id::text
       and DBT_INTERNAL_SOURCE.dbt_change_type::text in ('update'::text, 'delete'::text)
       and "poffertjes_shop"."poffertjes_snapshots"."order_items_snapshot".dbt_valid_to is null;
 
     insert into "poffertjes_shop"."poffertjes_snapshots"."order_items_snapshot" ("order_id", "product_id", "quantity", "created_at", "updated_at", "dbt_updated_at", "dbt_valid_from", "dbt_valid_to", "dbt_scd_id")
     select DBT_INTERNAL_SOURCE."order_id",DBT_INTERNAL_SOURCE."product_id",DBT_INTERNAL_SOURCE."quantity",DBT_INTERNAL_SOURCE."created_at",DBT_INTERNAL_SOURCE."updated_at",DBT_INTERNAL_SOURCE."dbt_updated_at",DBT_INTERNAL_SOURCE."dbt_valid_from",DBT_INTERNAL_SOURCE."dbt_valid_to",DBT_INTERNAL_SOURCE."dbt_scd_id"
-    from "order_items_snapshot__dbt_tmp131755219684" as DBT_INTERNAL_SOURCE
+    from "order_items_snapshot__dbt_tmp133721025129" as DBT_INTERNAL_SOURCE
     where DBT_INTERNAL_SOURCE.dbt_change_type::text = 'insert'::text;
 
   
--- run/poffertjes_shop/snapshots/__snapshot__orders.sql
+++ run/poffertjes_shop/snapshots/__snapshot__orders.sql
@@ -1,14 +1,14 @@
 
       update "poffertjes_shop"."poffertjes_snapshots"."orders_snapshot"
     set dbt_valid_to = DBT_INTERNAL_SOURCE.dbt_valid_to
-    from "orders_snapshot__dbt_tmp131755201634" as DBT_INTERNAL_SOURCE
+    from "orders_snapshot__dbt_tmp133721018835" as DBT_INTERNAL_SOURCE
     where DBT_INTERNAL_SOURCE.dbt_scd_id::text = "poffertjes_shop"."poffertjes_snapshots"."orders_snapshot".dbt_scd_id::text
       and DBT_INTERNAL_SOURCE.dbt_change_type::text in ('update'::text, 'delete'::text)
       and "poffertjes_shop"."poffertjes_snapshots"."orders_snapshot".dbt_valid_to is null;
 
     insert into "poffertjes_shop"."poffertjes_snapshots"."orders_snapshot" ("order_id", "customer_id", "shoppe_id", "created_at", "updated_at", "dbt_updated_at", "dbt_valid_from", "dbt_valid_to", "dbt_scd_id")
     select DBT_INTERNAL_SOURCE."order_id",DBT_INTERNAL_SOURCE."customer_id",DBT_INTERNAL_SOURCE."shoppe_id",DBT_INTERNAL_SOURCE."created_at",DBT_INTERNAL_SOURCE."updated_at",DBT_INTERNAL_SOURCE."dbt_updated_at",DBT_INTERNAL_SOURCE."dbt_valid_from",DBT_INTERNAL_SOURCE."dbt_valid_to",DBT_INTERNAL_SOURCE."dbt_scd_id"
-    from "orders_snapshot__dbt_tmp131755201634" as DBT_INTERNAL_SOURCE
+    from "orders_snapshot__dbt_tmp133721018835" as DBT_INTERNAL_SOURCE
     where DBT_INTERNAL_SOURCE.dbt_change_type::text = 'insert'::text;
 
   
