2a39803d33e2bc7fa7a27b289f4d97fe230c1c13
Tom Waterman <tjwaterman99@gmail.com>
2022-02-06 13:35:56 +0000

    Add more conventions

--- conventions.py
+++ conventions.py
@@ -1,3 +1,25 @@
+import pytest
+
+
+def test_sources_have_snapshots(source):
+    assert len(source.snapshots) == 1
+
+
+def test_base_models_map_to_sources_or_seeds(model):
+    if model.name.startswith('base_'):
+        assert len(model.parents) == 1
+        assert model.parents[0].resource_type in ('seed', 'source')
+        
+
+def test_base_models_are_in_base_directory(model):
+    if model.name.startswith('base_'):
+        assert model.path.startswith('base'), f"Invalid directory {model.path} for base model {model.name}"
+
+
+def test_sources_have_exactly_one_base_model(source):
+    child_models = [c.node for c in source.children if c.resource_type == 'model']
+    assert len(child_models) == 1
+    assert child_models[0].name.startswith('base')
 
 
 def test_selectors_have_descriptions(selector):
